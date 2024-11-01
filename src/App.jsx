1561e3dc45d12c471bbaad3150500cc4a1734214
usama_forayaje <usamaforayaje03@gmail.com>
2024-11-01 22:31:09 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque in suscipit repudiandae porro obcaecati nam exercitationem, excepturi magnam, soluta unde ex reiciendis possimus? Odit illo consequuntur quae, delectus mollitia incidunt.</h1>
+    </>
+  )
+}
+
+export default App
